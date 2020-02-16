9eac681bf78abe8edaa4b33358176731a48fa3e3
Juraj Andrassy <juraj.andrassy@gmail.com>
2020-02-16 10:39:02 +0100

    SDWebServer

---
+++ SDWebServer/SDWebServer.ino
@@ -0,0 +1,132 @@
+#include <UIPEthernet.h>
+#include <SD.h>
+#include <StreamLib.h> // install in Library Manager. Used to generate HTML of directory listing
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+const int SDCARD_CS = PA3;
+const int ETHERNET_CS = PA4;
+
+EthernetServer server(80);
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial);
+
+  pinMode(SDCARD_CS, OUTPUT);
+  digitalWrite(SDCARD_CS, HIGH);
+
+  // start the Ethernet connection:
+  Serial.println("Initialize Ethernet with DHCP:");
+  Ethernet.init(ETHERNET_CS);
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    while (true) {
+       delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+
+  if (!SD.begin(SDCARD_CS)) {
+    Serial.println(F("SD card initialization failed!"));
+    // don't continue
+    while (true);
+  }
+
+  server.begin();
+
+  IPAddress ip = Ethernet.localIP();
+  Serial.println();
+  Serial.println(F("Connected to WiFi network."));
+  Serial.print(F("To access the server, enter \"http://"));
+  Serial.print(ip);
+  Serial.println(F("/\" in web browser."));
+
+}
+
+void loop() {
+
+  static File file; // must by static to be accessible by the lambda functions
+  static char fn[32];
+
+  EthernetClient client = server.available();
+
+  if (client && client.connected()) {
+    if (client.find(' ')) { // GET /fn HTTP/1.1
+      int l = client.readBytesUntil(' ', fn, sizeof(fn) - 1); // read the filename from URL
+      fn[l] = 0;
+      client.find("\r\n\r\n");
+      file = SD.open(fn);
+      if (!file) { // file was not found
+        client.println(F("HTTP/1.1 404 Not Found"));
+        client.println(F("Connection: close"));
+        client.print(F("Content-Length: "));
+        client.println(strlen(" not found") + strlen(fn));
+        client.println();
+        client.print(fn);
+        client.print(F(" not found"));
+      } else if (file.isDirectory()) {
+        client.println(F("HTTP/1.1 200 OK"));
+        client.println(F("Connection: close"));
+        client.println(F("Content-Type: text/html"));
+        client.println(F("Transfer-Encoding: chunked"));
+        client.println();
+        char buff[64]; // buffer for chunks
+        ChunkedPrint chunked(client, buff, sizeof(buff));
+        chunked.begin();
+        chunked.printf(F("<!DOCTYPE html>\r\n<html>\r\n<body>\r\n<h3>Folder '%s'</h3>\r\n"), fn);
+        while (true) {
+          File entry = file.openNextFile();
+          if (!entry)
+            break;
+          if (strcmp(fn, "/") == 0) {
+            chunked.printf(F("<a href='%s'>"), entry.name());
+          } else {
+            chunked.printf(F("<a href='%s/%s'>"), fn, entry.name());
+          }
+          chunked.print(entry.name());
+          if (entry.isDirectory()) {
+            chunked.println(F("/</a><br>"));
+          } else {
+            chunked.printf(F("</a> (%ld b)<br>\r\n"), entry.size());
+          }
+          entry.close();
+        }
+        chunked.println(F("</body>\r\n</html>"));
+        chunked.end();
+      } else {
+          client.println(F("HTTP/1.1 200 OK"));
+          client.println(F("Connection: close"));
+          client.print(F("Content-Length: "));
+          client.println(file.size());
+          client.print(F("Content-Type: "));
+          const char* ext = strchr(file.name(), '.');
+          client.println(getContentType(ext));
+          client.println();
+        while (file.available()) {
+          client.write(file.read()); // send the file as body of the response
+        }
+        file.close();
+      }
+    }
+    client.stop();
+  }
+}
+
+const char* getContentType(const char* ext){
+  if (!strcmp(ext, ".HTM"))
+    return "text/html";
+  if (!strcmp(ext, ".CSS"))
+    return "text/css";
+  if (!strcmp(ext, ".JS"))
+    return "application/javascript";
+  if (!strcmp(ext, ".PNG"))
+    return "image/png";
+  if (!strcmp(ext, ".GIF"))
+    return "image/gif";
+  if (!strcmp(ext, ".JPG"))
+    return "image/jpeg";
+  if (!strcmp(ext, ".XML"))
+    return "text/xml";
+  return "text/plain";
+}
